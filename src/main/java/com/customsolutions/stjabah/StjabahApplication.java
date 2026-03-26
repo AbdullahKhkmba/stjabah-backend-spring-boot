72da5b7f0482279bcd7f1275fde16fe0e0febdc6
Abdullah Ayman <84142402+AbdullahKhkmba@users.noreply.github.com>
2026-03-26 16:56:48 +0200

    initial commit

---
+++ src/main/java/com/customsolutions/stjabah/StjabahApplication.java
@@ -0,0 +1,13 @@
+package com.customsolutions.stjabah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StjabahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StjabahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customsolutions/stjabah/StjabahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customsolutions.stjabah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StjabahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
